c93208f9c14ce0dfbd10e03607c53313ec841ce9
Bhavesh Yadav <codezera3@gmail.com>
2023-07-14 22:48:06 +0530

    added stickers tshirts mugs and hoodies page

---
+++ app/tshirts/page.js
@@ -0,0 +1,138 @@
+import Image from "next/image"
+
+const Tshirts = () => {
+  return (
+    <div className='flex flex-col '>
+        <h1 className='text-5xl font-semibold text-center mt-6'>Tshirts</h1>
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-16 mx-auto">
+                <div class="flex flex-wrap -m-4 gap-2">
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <Image alt="ecommerce" class=" object-top w-full h-full block" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width={420} height={260} />
+                    </a>
+                    <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRT</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                    <p class="mt-1">₹499</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+  )
+}
+
+export default Tshirts
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,14 +5,14 @@ import  { GrCart } from 'react-icons/gr'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between w-full bg-gray-700'>
+    <div className='flex justify-between w-full bg-gray-700 shadow-lg mb-1'>
         <div className='logo flex items-center justify-center space-x-5'>
             <Link href={'/'}><Image src={'/logo/logo6.png'} width={70} height={30} /></Link>
             <ul className='text-white flex space-x-5'>
-               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/'}><li>Tshirts</li></Link>
-               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/'}><li>Hoodies</li></Link>
-               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/'}><li>Stickers</li></Link>
-               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/'}><li>Mugs</li></Link>
+               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/tshirts'}><li>Tshirts</li></Link>
+               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/hoodies'}><li>Hoodies</li></Link>
+               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/stickers'}><li>Stickers</li></Link>
+               <Link className='cursor-pointer hover:text-[#00A4BF]'  href={'/mugs'}><li>Mugs</li></Link>
             </ul>
         </div>
         <div className='cart cursor-pointer flex justify-center items-center mr-7'>
